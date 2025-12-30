545b23f7ada379b9b0ddc55f455d9c9dd74b84cf
Nicolas <bn0099@mci4me.at>
2025-12-30 11:18:45 +0100

    fix merge conflict

--- gesamtprojekt/src/main/java/com/gesamtprojekt/application/ui/client/calendar/CalendarView.java
+++ gesamtprojekt/src/main/java/com/gesamtprojekt/application/ui/client/calendar/CalendarView.java
@@ -3,15 +3,15 @@ package com.gesamtprojekt.application.ui.client.calendar;
 import com.gesamtprojekt.application.ui.client.MainLayout;
 import com.gesamtprojekt.application.ui.components.calendar.CalendarControlsBar;
 import com.gesamtprojekt.application.ui.components.calendar.CalendarRoomsSection;
-import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.auth.AnonymousAllowed;
+import jakarta.annotation.security.PermitAll;
+
 
 @Route(value = "calendar", layout = MainLayout.class)
 @PageTitle("Calendar")
-@AnonymousAllowed
+@PermitAll
 public class CalendarView extends VerticalLayout {
 
     public CalendarView() {
@@ -26,15 +26,5 @@ public class CalendarView extends VerticalLayout {
 
         add(controls, rooms);
         setFlexGrow(1, rooms);
-import jakarta.annotation.security.PermitAll;
-
-@Route(value = "calendar", layout = MainLayout.class)
-@PageTitle("Calendar")
-@PermitAll
-public class CalendarView extends VerticalLayout {
-
-    public CalendarView() {
-
-        add(new H1("Calendar"));
     }
 }
--- gesamtprojekt/src/main/java/com/gesamtprojekt/application/ui/client/dashboard/DashboardView.java
+++ gesamtprojekt/src/main/java/com/gesamtprojekt/application/ui/client/dashboard/DashboardView.java
@@ -7,13 +7,11 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.auth.AnonymousAllowed;
+import jakarta.annotation.security.PermitAll;
 
-//@RolesAllowed({"USER", "ADMIN"})
-@Route(value = "dashboard", layout = MainLayout.class)
+@Route(value = "", layout = MainLayout.class)
 @PageTitle("Dashboard")
-@AnonymousAllowed
-
+@PermitAll
 public class DashboardView extends VerticalLayout {
 
     public DashboardView() {
@@ -43,20 +41,7 @@ public class DashboardView extends VerticalLayout {
 
         return layout;
     }
-
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.PageTitle;
-import com.vaadin.flow.router.Route;
-import jakarta.annotation.security.PermitAll;
-
-@Route(value = "", layout = MainLayout.class)
-@PageTitle("Dashboard")
-@PermitAll
-public class DashboardView extends VerticalLayout {
-
-    public DashboardView() {
-
-        add(new H1("Dashboard"));
-    }
 }
+
+
+
